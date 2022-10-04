17c489871262a77cc4bfada0c1007d0f498d7fce
Hubert Pietrzykowski <pietrzykowskihubert@gmail.com>
2022-10-04 14:15:15 +0200

    deposits now use U128

--- marketplace/src/deposit.rs
+++ marketplace/src/deposit.rs
@@ -1,12 +1,10 @@
-use crate::{
-    *,
-    constants::{MIN_DEPOSIT},
-};
+use crate::{constants::MIN_DEPOSIT, *};
+use near_sdk::json_types::U128;
 
 #[near_bindgen]
 impl MarketplaceContract {
     #[payable]
-    pub fn place_deposit(&mut self) {
+    pub fn place_deposit(&mut self) -> U128 {
         let account_id = env::predecessor_account_id();
         let deposited_amount_yocto = env::attached_deposit();
 
@@ -17,30 +15,32 @@ impl MarketplaceContract {
             "Please deposit at least {}",
             MIN_DEPOSIT - current_deposit
         );
-        self.storage_deposits
-            .insert(&account_id, &(current_deposit + deposited_amount_yocto));
+        let updated_deposit = current_deposit + deposited_amount_yocto;
+        self.storage_deposits.insert(&account_id, &updated_deposit);
+        return U128(updated_deposit);
     }
 
-    pub fn withdraw_deposit(&mut self, withdrawn_amount_yocto: Balance) {
+    pub fn withdraw_deposit(&mut self, withdrawn_amount_yocto: U128) -> U128 {
         let account_id = env::predecessor_account_id();
-
+        let withdrawn_balance: Balance = withdrawn_amount_yocto.0;
         // get current deposit for this account
         let current_deposit: Balance = self.storage_deposits.get(&account_id).unwrap_or(0);
         assert!(
-            withdrawn_amount_yocto <= current_deposit,
+            withdrawn_balance <= current_deposit,
             "Withdrawn amount exceeds your current deposit of {}",
             current_deposit
         );
-        self.storage_deposits
-            .insert(&account_id, &(current_deposit - withdrawn_amount_yocto));
-        Promise::new(account_id).transfer(withdrawn_amount_yocto);
+        let updated_deposit = current_deposit - withdrawn_balance;
+        self.storage_deposits.insert(&account_id, &updated_deposit);
+        Promise::new(account_id).transfer(withdrawn_balance);
+        return U128(updated_deposit);
     }
 
-    pub fn deposit(&self, account_id: AccountId) -> (Balance, bool) {
+    pub fn deposit(&self, account_id: AccountId) -> (U128, bool) {
         if let Some(current_deposit) = self.storage_deposits.get(&account_id) {
-            (current_deposit, true)
+            (U128(current_deposit), true)
         } else {
-            (0, false)
+            (U128(0), false)
         }
     }
 }
